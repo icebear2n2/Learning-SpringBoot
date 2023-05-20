a4a0f491773bc1481550ebb8608de2411a886cef
icebear2n2 <icebear2n2@gmail.com>
2023-05-20 11:51:22 +0900

    API 작성

---
+++ src/main/java/com/springboot/hello/controller/GetController.java
@@ -0,0 +1,56 @@
+package com.springboot.hello.controller;
+
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiParam;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Map;
+
+@RestController
+@RequestMapping("/api/v1/get-api")
+public class GetController {
+
+    private final Logger LOGGER = LoggerFactory.getLogger(GetController.class);
+
+    @RequestMapping(value = "/hello", method = RequestMethod.GET)
+    public String getHello() {
+        LOGGER.info("getHello 메서드가 호출되었습니다");
+        return "Hello World!";
+    }
+
+    @GetMapping(value = "/name")
+    public String getName() {
+        LOGGER.info("getName 메서드가 호출되었습니다");
+        return "sujeong";
+    }
+
+    @GetMapping(value = "/variable1/{variable}")
+    public String getVariable1(@PathVariable String variable) {
+        LOGGER.info("@PathVariable을 통해 들어온 값 : {}", variable);
+        return variable;
+    }
+
+    @ApiOperation(value = "GET 메서드 예제", notes = "@RequestParam을 활용한 GET Method")
+    @GetMapping(value = "/request1")
+    public String getRequestParam1(
+            @ApiParam(value = "이름", required = true) @RequestParam String name,
+            @ApiParam(value = "이메일", required = true) @RequestParam String email,
+            @ApiParam(value = "회사", required = true) @RequestParam String organization
+    ) {
+        return name + " " + email + " " + organization;
+    }
+
+    @GetMapping(value = "/request2")
+    public String getRequestParam2(@RequestParam Map<String, String> param) {
+        StringBuilder sb = new StringBuilder();
+
+        param.entrySet().forEach(map -> {
+            sb.append(map.getKey() + " : " + map.getValue() + "\n");
+        });
+
+        return sb.toString();
+    }
+
+ }
